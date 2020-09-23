49f77aeac40ec7baf7f45572daddee2d02bda662
alexander <alexander31777@gmail.com>
2020-09-23 14:40:57 +0300

    Initial commit

---
+++ src/ru/mirea/lab4/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.lab4;
+
+public class Main {
+    public static void main(String[] args) {
+        Matches matches = new Matches();
+        matches.setVisible(true);
+    }
+}
---
+++ src/ru/mirea/lab4/Matches.java
@@ -0,0 +1,123 @@
+package ru.mirea.lab4;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Matches extends JFrame{
+    private JButton button1 = new JButton("AC Milan");
+    private JButton button2 = new JButton("Real Madrid");
+    private JButton button3 = new JButton("Complete the match");
+    private JLabel label1 = new JLabel("Result: ");
+    private JLabel label2 = new JLabel("Last Scorer: N/A");
+    private JLabel label3 = new JLabel("Winner: N/A");
+    private int milan = 0;
+    private int madrid = 0;
+
+    Timer timer = new Timer();
+    TimerTask exitApp = new TimerTask() {
+        public void run() {
+            System.exit(0);
+        }
+    };
+
+    public Matches(){
+        this.setBounds(200,200,700,500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = this.getContentPane();
+        container.setLayout(new GridLayout(6,BoxLayout.Y_AXIS));
+
+        container.add(label1);
+        container.add(label2);
+        container.add(label3);
+        container.add(button1);
+        container.add(button2);
+        container.add(button3);
+        label1.setVerticalAlignment(JLabel.TOP);
+        label1.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+        label1.setPreferredSize(new Dimension(250, 100));
+        label1.setForeground(new Color(40, 40, 40));
+
+        label2.setVerticalAlignment(JLabel.TOP);
+        label2.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+        label2.setPreferredSize(new Dimension(250, 100));
+        label1.setForeground(new Color(40, 40, 40));
+
+        label3.setVerticalAlignment(JLabel.TOP);
+        label3.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+        label3.setPreferredSize(new Dimension(250, 100));
+        label1.setForeground(new Color(40, 40, 40));
+
+        button1.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                milan+=1;
+                label1.setText("Result: "+milan+" X "+madrid);
+                label2.setText("Last scorer: AC Milan");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) { }
+
+            @Override
+            public void mouseReleased(MouseEvent e) { }
+
+            @Override
+            public void mouseEntered(MouseEvent e) { }
+
+            @Override
+            public void mouseExited(MouseEvent e) { }
+        });
+        button2.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                madrid+=1;
+                label1.setText("Result: "+milan+" X "+madrid);
+                label2.setText("Last scorer: Real Madrid");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) { }
+
+            @Override
+            public void mouseReleased(MouseEvent e) { }
+
+            @Override
+            public void mouseEntered(MouseEvent e) { }
+
+            @Override
+            public void mouseExited(MouseEvent e) { }
+        });
+        button3.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (milan > madrid){
+                    label3.setText("Winner: AC Milan");
+                }
+                else if (madrid > milan){
+                    label3.setText("Winner: Real Madrid");
+                }
+                else {
+                    label3.setText("Winner: DRAW");
+                }
+                timer.schedule(exitApp, new Date(System.currentTimeMillis()+3*1000));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) { }
+
+            @Override
+            public void mouseReleased(MouseEvent e) { }
+
+            @Override
+            public void mouseEntered(MouseEvent e) { }
+
+            @Override
+            public void mouseExited(MouseEvent e) { }
+        });
+    }
+}
